bbf351db8be2a3bded6de31248995938a2a37193
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-03-27 18:18:07 +0200

    t

--- src/skribi/program.py
+++ src/skribi/program.py
@@ -4,14 +4,34 @@
 
 class Program:
 
-    def __init__(self):
-        self.cursor = None
-
     def execute(self, file):
         """
         Execute the file
         :param file:
         :return:
         """
-        self.cursor = -1
         pass
+
+
+# ====== #
+# tokens #
+# ====== #
+
+TT_INT = 'TT_INT'
+TT_FLOAT = 'FLOAT'
+TT_PLUS = 'PLUS'
+TT_PLUS_DIBI = 'PLUS_DIBI'
+
+class Token:
+
+    def __init__(self, type_, value):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        if self.value: return f'{self.type}:{self.value}'
+        return f'{self.type}'
+
+# ============== #
+# class of LEXER #
+# ============== #
