848d48ef3c2c7dbf8c2cdb2e65143ccee56856f6
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-06 15:03:06 +0200

    parsing by Copilot

--- src/skribi/parser.py
+++ src/skribi/parser.py
@@ -53,6 +53,71 @@ class OperatorNode(object):
         else:
             return SkribiException("Unknown operator: " + str(self.token.value), "evaluation")
 
+
 # --------------------------------------------------------------------------- #
 # Parser                                                                      #
 # --------------------------------------------------------------------------- #
+
+# Parser class
+class Parser:
+
+    def __init__(self):
+        self.tokens = []
+        self.index = 0
+        self.current_token = None
+        self.current_node = None
+        self.current_line = None
+        self.current_column = None
+
+    # Parse
+    def parse(self, tokens: list):
+        self.tokens = tokens
+        self.index = 0
+        self.current_token = None
+        self.current_node = None
+        self.current_line = None
+        self.current_column = None
+        self.next_token()
+        return self.parse_expr()
+
+    # Parse expression
+    def parse_expr(self):
+        self.current_node = self.parse_term()
+        while self.current_token.value in ["+", "-", "*", "/", "^"]:
+            self.current_node = OperatorNode(self.current_token, self.current_node, self.parse_term())
+            self.next_token()
+        return self.current_node
+
+    # Parse term
+    def parse_term(self):
+        self.current_node = self.parse_factor()
+        while self.current_token.value in ["+", "-", "*", "/", "^"]:
+            self.current_node = OperatorNode(self.current_token, self.current_node, self.parse_factor())
+            self.next_token()
+        return self.current_node
+
+    # Parse factor
+    def parse_factor(self):
+        if self.current_token.type == "number":
+            self.current_node = NumberNode(self.current_token)
+            self.next_token()
+        elif self.current_token.type == "left_parenthesis":
+            self.next_token()
+            self.current_node = self.parse_expr()
+            if self.current_token.type == "right_parenthesis":
+                self.next_token()
+            else:
+                raise SkribiException("Missing right parenthesis", "parsing")
+        else:
+            raise SkribiException("Unexpected token: " + str(self.current_token.value), "parsing")
+        return self.current_node
+
+    def next_token(self):
+        if self.index >= len(self.tokens):
+            self.current_token = Token(None, None)
+            return
+        self.current_token = self.tokens[self.index]
+        self.index += 1
+        self.current_line = self.current_token.line
+        self.current_column = self.current_token.column
+        return
