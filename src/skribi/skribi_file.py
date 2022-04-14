83945a74e825718a7e443627220184264a25d5c6
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-04-14 17:28:40 +0200

    début scopes

--- src/skribi/skribi_file.py
+++ src/skribi/skribi_file.py
@@ -1,3 +1,29 @@
+from variables_and_types import *
+from custom_exception import *
+
+
+# ================================== #
+# element that can contain variables #
+# ================================== #
+
+class ContainVariables:
+
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.variables = {}
+
+    def set_variable(self, name: str, variable):
+        self.variables[name] = variable
+
+    def get_variable(self, name: str, current_scope):
+        if name in self.variables:
+            return self.variables[name]
+        elif self.parent:
+            return self.parent.get_variable(name)
+        else:
+            return SkribiException("Variable '{}' not found".format(name), "interpreter", current_scope.trace())
+
+
 # ====================== #
 # class of a Skribi file #
 # ====================== #
