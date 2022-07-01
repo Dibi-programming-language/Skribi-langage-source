09dff70a63134bcea426f46af756646647fc4a04
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2022-07-01 12:05:02 +0200

    Fix #17

--- src/skribi/parser.py
+++ src/skribi/parser.py
@@ -110,13 +110,8 @@ class VariableNode(ExecutableNode):
             pass
 
 
-class ScopeNodeTemp:
-    def get_node(self, index):
-        pass
-
-
-class ScopeNode(ScopeNodeTemp):
-    def __init__(self, nodes: list[ExecutableNode | ScopeNodeTemp], token):
+class ScopeNode:
+    def __init__(self, nodes: list[ExecutableNode | 'ScopeNode'], token):
         super().__init__(token)
         self.nodes = nodes
 
