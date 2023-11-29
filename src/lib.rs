69e42e7ad3281d60bce1f852a0e81beda0d6a4df
Rignchen <rinchendemort@gmail.com>
2023-11-29 10:34:38 +0100

    use rustfmt to format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 use std::process::Command;
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
 
 pub fn clear() {
     match Command::new("clear").status() {
-        Ok(_) => {},
-        Err(_) => {
-            match Command::new("cl").status() {
-                Ok(_) => {},
-                Err(_) => {
-                    for _ in 0..100 {
-                        println!()
-                    }
-                },
+        Ok(_) => {}
+        Err(_) => match Command::new("cls").status() {
+            Ok(_) => {}
+            Err(_) => {
+                for _ in 0..100 {
+                    println!()
+                }
             }
         },
     }
