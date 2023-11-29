9acdfe01dcf981f6e2871dda57bbdf54010bb3f4
Rignchen <rinchendemort@gmail.com>
2023-11-29 10:19:00 +0100

    add flag support

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,17 @@ use skribi_language_source::clear;
 
 // Main
 fn main() {
+
     let args: Vec<_> = env::args().collect(); // get the command line arguments
     let mut path = String::new();
     let extension: Vec<String> = vec!["sk".to_string(), "skribi".to_string()];
+    let flag_char = "/"; // if it was "-", it would sometimes interfere with cargo's flags
+
+    // clear the shell for the user
+    if !args.contains(&String::from(flag_char.to_string() + "interpret-debug")) {clear();}
 
     // Get the path of the file to run
-    if args.len() > 1 {
+    if args.len() > 1 && !args[1].starts_with(flag_char) {
         path = args[1].clone();
     } else {
         println!("Enter a file to run");
