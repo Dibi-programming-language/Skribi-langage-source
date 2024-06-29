3183ad6cf43732ff1417f2da6b447293874661f8
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2024-06-29 14:24:18 +0200

    End of if_else.rs

--- src/parse/nodes/if_else.rs
+++ src/parse/nodes/if_else.rs
@@ -2,7 +2,7 @@ use std::collections::VecDeque;
 use crate::impl_debug;
 use crate::parse::nodes::expressions::Exp;
 use crate::parse::nodes::GraphDisplay;
-use crate::skr_errors::ResultOption;
+use crate::skr_errors::{CustomError, ResultOption};
 use crate::tokens::Token;
 
 // Grammar for this file:
@@ -47,6 +47,55 @@ pub enum Sula {
     Scope(Scope)
 }
 
+impl GraphDisplay for Sula {
+    fn graph_display(&self, graph: &mut String, id: &mut usize) {
+        graph.push_str(&format!("\nsubgraph Sula_{}[Sula]", id));
+        *id += 1;
+        match self {
+            Sula::Ij { ij, sula } => {
+                ij.graph_display(graph, id);
+                if let Some(sula) = sula {
+                    sula.graph_display(graph, id);
+                }
+            }
+            Sula::Scope(scope) => {
+                scope.graph_display(graph, id);
+            }
+        }
+    }
+}
+
+impl_debug!(Sula);
+
+impl Sula {
+    pub fn new(ij: Ij, sula: Option<Sula>) -> Self {
+        Self::Ij {
+            ij,
+            sula: sula.map(Box::new),
+        }
+    }
+    
+    pub fn parse(tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
+        // <sula> ::= sula (<ij> (<sula> |) | <scope>)
+        if let Some(Token::KeywordElse) = tokens.front() {
+            tokens.pop_front();
+            if let Some(ij) = Ij::parse(tokens)? {
+                if let Some(sula) = Sula::parse(tokens)? {
+                    Ok(Some(Sula::Ij { ij, sula: Some(Box::new(sula)) }))
+                } else {
+                    Err(CustomError::UnexpectedToken("Expected a sula".to_string()))
+                }
+            } else if let Some(scope) = Scope::parse(tokens)? {
+                Ok(Some(Sula::Scope(scope)))
+            } else {
+                Err(CustomError::UnexpectedToken("Expected an ij or a scope".to_string()))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
 // ----------
 // --- Ij ---
 // ----------
@@ -57,6 +106,42 @@ pub struct Ij {
     scope: Scope,
 }
 
+impl GraphDisplay for Ij {
+    fn graph_display(&self, graph: &mut String, id: &mut usize) {
+        graph.push_str(&format!("\nsubgraph Ij_{}[Ij]", id));
+        *id += 1;
+        self.exp.graph_display(graph, id);
+        self.scope.graph_display(graph, id);
+    }
+}
+
+impl_debug!(Ij);
+
+impl Ij {
+    pub fn new(exp: Exp, scope: Scope) -> Self {
+        Self { exp, scope }
+    }
+    
+    pub fn parse(tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
+        // <ij> ::= ij <exp> <scope>
+        if let Some(Token::KeywordIf) = tokens.front() {
+            tokens.pop_front();
+            match Exp::parse(tokens) {
+                Some(Ok(exp)) => {
+                    match Scope::parse(tokens)? {
+                        Some(scope) => Ok(Some(Ij::new(exp, scope))),
+                        None => Err(CustomError::UnexpectedToken("Expected a scope".to_string()))
+                    }
+                }
+                Some(Err(err)) => Err(err),
+                None => Err(CustomError::UnexpectedToken("Expected an expression".to_string()))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+}
+
 // ------------
 // --- Cond ---
 // ------------
@@ -65,4 +150,36 @@ pub struct Ij {
 pub struct Cond {
     ij: Ij,
     sula: Option<Box<Sula>>,
+}
+
+impl GraphDisplay for Cond {
+    fn graph_display(&self, graph: &mut String, id: &mut usize) {
+        graph.push_str(&format!("\nsubgraph Cond_{}[Cond]", id));
+        *id += 1;
+        self.ij.graph_display(graph, id);
+        if let Some(sula) = &self.sula {
+            sula.graph_display(graph, id);
+        }
+    }
+}
+
+impl_debug!(Cond);
+
+impl Cond {
+    pub fn new(ij: Ij, sula: Option<Sula>) -> Self {
+        Self { ij, sula: sula.map(Box::new) }
+    }
+    
+    pub fn parse(tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
+        // <cond> ::= <ij> (<sula> |)
+        if let Some(ij) = Ij::parse(tokens)? {
+            if let Some(sula) = Sula::parse(tokens)? {
+                Ok(Some(Cond::new(ij, Some(sula))))
+            } else {
+                Ok(Some(Cond::new(ij, None)))
+            }
+        } else {
+            Ok(None)
+        }
+    }
 }
