01822c2b7fe5f699d4d109fa22541aff4c75a9c4
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2024-06-29 18:18:29 +0200

    Add docu for if_else.rs

--- src/parse/nodes/if_else.rs
+++ src/parse/nodes/if_else.rs
@@ -1,9 +1,9 @@
-use std::collections::VecDeque;
 use crate::impl_debug;
 use crate::parse::nodes::expressions::Exp;
 use crate::parse::nodes::GraphDisplay;
 use crate::skr_errors::{CustomError, ResultOption};
 use crate::tokens::Token;
+use std::collections::VecDeque;
 
 // Grammar for this file:
 // <sula> ::= sula (<ij> (<sula> |) | <scope>)
@@ -27,7 +27,7 @@ impl Scope {
     pub fn new() -> Self {
         Self {}
     }
-    
+
     pub fn parse(_tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
         // TODO
         Ok(Some(Scope::new()))
@@ -38,13 +38,25 @@ impl Scope {
 // --- Sula ---
 // ------------
 
+/// `Sula` represents an "else" block in the AST. It can contain an [Ij] if this is an "else if"
+/// block, or a [Scope] if this is an "else" block. It there is an "else if" block, it can contain
+/// another [Sula].
+/// 
+/// The principe is to have a [Cond] node that contains an [Ij] and an optional [Sula]. The [Sula]
+/// chains the "else if" blocks and the "else" block, while [Ij] contains the condition and the
+/// block to execute if the condition is true.
+/// 
+/// The [Sula] node is recursive, as it can contain another [Sula] node.
+/// 
+/// # Grammar
+/// 
+/// `<sula> ::= sula (<ij> (<sula> |) | <scope>)`
+/// 
+/// See also [Ij] and [Scope].
 #[derive(PartialEq)]
 pub enum Sula {
-    Ij {
-        ij: Ij,
-        sula: Option<Box<Sula>>,
-    },
-    Scope(Scope)
+    Ij { ij: Ij, sula: Option<Box<Sula>> },
+    Scope(Scope),
 }
 
 impl GraphDisplay for Sula {
@@ -74,21 +86,26 @@ impl Sula {
             sula: sula.map(Box::new),
         }
     }
-    
+
     pub fn parse(tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
         // <sula> ::= sula (<ij> (<sula> |) | <scope>)
         if let Some(Token::KeywordElse) = tokens.front() {
             tokens.pop_front();
             if let Some(ij) = Ij::parse(tokens)? {
                 if let Some(sula) = Sula::parse(tokens)? {
-                    Ok(Some(Sula::Ij { ij, sula: Some(Box::new(sula)) }))
+                    Ok(Some(Sula::Ij {
+                        ij,
+                        sula: Some(Box::new(sula)),
+                    }))
                 } else {
                     Err(CustomError::UnexpectedToken("Expected a sula".to_string()))
                 }
             } else if let Some(scope) = Scope::parse(tokens)? {
                 Ok(Some(Sula::Scope(scope)))
             } else {
-                Err(CustomError::UnexpectedToken("Expected an ij or a scope".to_string()))
+                Err(CustomError::UnexpectedToken(
+                    "Expected an ij or a scope".to_string(),
+                ))
             }
         } else {
             Ok(None)
@@ -100,6 +117,15 @@ impl Sula {
 // --- Ij ---
 // ----------
 
+/// `Ij` represents an "if" block in the AST. It contains an [Exp] and a [Scope]. It can be followed
+/// by a [Sula] IN ITS PARENT NODE, to represent an "else if" block or an "else" block. The [Ij]
+/// node itself is not recursive (well ... unless there is another "if" block in the [Scope]).
+/// 
+/// # Grammar
+/// 
+/// `<ij> ::= ij <exp> <scope>`
+/// 
+/// See also [Exp] and [Scope].
 #[derive(PartialEq)]
 pub struct Ij {
     exp: Exp,
@@ -121,20 +147,20 @@ impl Ij {
     pub fn new(exp: Exp, scope: Scope) -> Self {
         Self { exp, scope }
     }
-    
+
     pub fn parse(tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
         // <ij> ::= ij <exp> <scope>
         if let Some(Token::KeywordIf) = tokens.front() {
             tokens.pop_front();
             match Exp::parse(tokens) {
-                Some(Ok(exp)) => {
-                    match Scope::parse(tokens)? {
-                        Some(scope) => Ok(Some(Ij::new(exp, scope))),
-                        None => Err(CustomError::UnexpectedToken("Expected a scope".to_string()))
-                    }
-                }
+                Some(Ok(exp)) => match Scope::parse(tokens)? {
+                    Some(scope) => Ok(Some(Ij::new(exp, scope))),
+                    None => Err(CustomError::UnexpectedToken("Expected a scope".to_string())),
+                },
                 Some(Err(err)) => Err(err),
-                None => Err(CustomError::UnexpectedToken("Expected an expression".to_string()))
+                None => Err(CustomError::UnexpectedToken(
+                    "Expected an expression".to_string(),
+                )),
             }
         } else {
             Ok(None)
@@ -146,6 +172,15 @@ impl Ij {
 // --- Cond ---
 // ------------
 
+/// `Cond` is the starting node for an "if" block in the AST. It contains an [Ij] and an optional
+/// [Sula]. The [Sula] is used to chain "else if" blocks and "else" blocks. [Cond] allows to use
+/// any chain of "if", "else if" and "else" blocks in the same way.
+/// 
+/// # Grammar
+/// 
+/// `<cond> ::= <ij> (<sula> |)`
+/// 
+/// See also [Ij] and [Sula].
 #[derive(PartialEq)]
 pub struct Cond {
     ij: Ij,
@@ -167,9 +202,12 @@ impl_debug!(Cond);
 
 impl Cond {
     pub fn new(ij: Ij, sula: Option<Sula>) -> Self {
-        Self { ij, sula: sula.map(Box::new) }
+        Self {
+            ij,
+            sula: sula.map(Box::new),
+        }
     }
-    
+
     pub fn parse(tokens: &mut VecDeque<Token>) -> ResultOption<Self> {
         // <cond> ::= <ij> (<sula> |)
         if let Some(ij) = Ij::parse(tokens)? {
