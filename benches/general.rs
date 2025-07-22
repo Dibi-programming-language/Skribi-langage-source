0014072851cd00cbdabe0214f25083edcbc644e6
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2025-07-22 20:10:08 +0200

    Fmt

--- benches/general.rs
+++ benches/general.rs
@@ -4,21 +4,17 @@ use skribi_language_source::execute;
 macro_rules! create_benchmark {
     ($c: expr, $name: expr, $path: expr) => {
         $c.bench_function($name, |b| {
-            b.iter(|| {
-                execute(
-                    vec![
-                    "".to_owned(),
-                    $path.to_owned(),
-                    ],
-                    false,
-                )
-            })
+            b.iter(|| execute(vec!["".to_owned(), $path.to_owned()], false))
         });
     };
 }
 
 pub fn criterion_general_benchmark(c: &mut Criterion) {
-    create_benchmark!(c, "fibo 19 skribi", "resources/test_programs/algo/fibo.skrb");
+    create_benchmark!(
+        c,
+        "fibo 19 skribi",
+        "resources/test_programs/algo/fibo.skrb"
+    );
     create_benchmark!(c, "or_eq skribi", "resources/test_programs/cmp/or_eq.skrb");
 }
 
