327ef3b9f4c580b57a863c38c1ee88c1c6e99743
Alexis <>
2026-07-20 15:07:22 +0200

    add(clap&log): use clap to configure a simple logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+use log::trace;
 use miette::{Context, Report, Result};
 
 pub fn execute() -> Result<()> {
+    trace!("Entenring execute");
     Err(Report::msg("Rework in progress."))
         .context("Skribi is not usable right now.")
 }
--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,41 @@
 // Skribi's shell //
 ////////////////////
 
+use clap::Parser;
+
+use log::trace;
 use miette::{Context, Result};
 
 use skribi::execute;
 
+/// The Skribi compiler CLI
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Arguments {
+    /// Log more information, set the level to INFO.
+    /// For fine-grained control over log levels, use the RUST_LOG variable.
+    #[arg(short, long)]
+    verbose: bool,
+    /// Log all information, set the level to TRACE.
+    /// For fine-grained control over log levels, use the RUST_LOG variable.
+    #[arg(long)]
+    very_verbose: bool,
+}
 
 /// Launch the interpreter
 fn main() -> Result<()> {
+    let args = Arguments::parse();
+
+    let mut logger = env_logger::Builder::from_default_env();
+    if args.verbose {
+        logger.filter_level(log::LevelFilter::Info);
+    } else if args.very_verbose {
+        logger.filter_level(log::LevelFilter::Trace);
+    }
+    logger.init();
+
+    trace!("Logger initialised, entenring main");
+
     execute()
         .context("Failed to execute your file.")
 }
