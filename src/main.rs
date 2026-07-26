4241027798d6e7cfc7043a6c264b999ff5353fd1
Alexis <>
2026-07-26 21:47:44 +0200

    fix(logger): change variable name

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@
 // Skribi's shell //
 ////////////////////
 
-use std::env::var_os;
-
 use clap::Parser;
 
+use env_logger::{Builder, Env};
 use log::{LevelFilter, trace};
 use miette::{Context, Result};
 
@@ -19,8 +18,9 @@ use skribi::execute;
 struct Arguments {
     /// Log more information. Fine-grained control.
     ///
-    /// The RUST_LOG variable can also be used.
-    /// The variable is overrided by the argument.
+    /// The SKRIBI_C_LOG variable can also be used.
+    /// To specify a style, use SKRIBI_C_LOG_STYLE.
+    /// The variable is overriden by the argument.
     ///
     /// With nothing set, defaults to warn.
     ///
@@ -33,12 +33,23 @@ struct Arguments {
 fn main() -> Result<()> {
     let args = Arguments::parse();
 
-    let mut logger = env_logger::Builder::from_default_env();
+    let mut logger = Builder::from_env(
+        Env::default()
+            .filter_or("SKRIBI_C_LOG", "warn")
+            .write_style("SKRIBI_C_LOG_STYLE"),
+    );
+
+    // To ignore the env variable in production:
+    // #[cfg(not (debug_assertions))]
+    // logger.filter_level(LevelFilter::Warn);
+    // Or detect special values?
+    // Did not add the env to clap as this would be a double with from_env
+    // in debug mode.
+
     if let Some(level) = args.verbose {
         logger.filter_level(level);
-    } else if var_os("RUST_LOG").is_none() {
-        logger.filter_level(LevelFilter::Warn);
     }
+
     logger.init();
 
     trace!("Logger initialised, entenring main");
